876d74d8983475af1759ee976c49ac1c7c27f524
Pfeszler <pfeszler@gmail.com>
2021-01-25 10:53:13 +0100

    added action to toggle Dark Mode

--- src/features/homepage/common/Header/DarkModeButton/index.js
+++ src/features/homepage/common/Header/DarkModeButton/index.js
@@ -1,25 +1,28 @@
 import React from "react"
 import { Button, Icon, Label, Slider, Wrapper } from "./styled"
 import image from "../../assets/darkModeButton.svg"
-import { useSelector } from "react-redux"
-import { selectDarkMode } from "../../../pageSlice"
+import { useDispatch, useSelector } from "react-redux"
+import { selectDarkMode, toggleDarkMode } from "../../../pageSlice"
 
 
 const DarkModeButton = () => {
 
     const darkMode = useSelector(selectDarkMode);
+    const dispatch = useDispatch();
 
     return (
         <Wrapper>
             <Label>
                 dark mode {darkMode ? "on" : "off"}
             </Label>
-            <Slider>
+            <Slider
+                onClick={() => dispatch(toggleDarkMode())}
+            >
                 <Button>
-                    <Icon 
-                    src={image}
-                    darkMode = {darkMode}
-                     />
+                    <Icon
+                        src={image}
+                        darkMode={darkMode}
+                    />
                 </Button>
             </Slider>
         </Wrapper>
