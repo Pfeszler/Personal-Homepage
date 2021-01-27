489d98b3317c464a7265c2fe26e4953b87a99806
Pfeszler <pfeszler@gmail.com>
2021-01-27 11:47:45 +0100

    Portfolio section created

---
+++ src/features/homepage/common/Portfolio/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import PortfolioHeader from "./PortfolioHeader"
+import { StyledPortfolio } from "./styleds"
+
+const Portfolio = () => (
+    <StyledPortfolio>
+        <PortfolioHeader />
+    </StyledPortfolio>
+)
+
+export default Portfolio
---
+++ src/features/homepage/common/Portfolio/styleds.js
@@ -0,0 +1,5 @@
+import styled from "styled-components"
+
+export const StyledPortfolio = styled.section`
+    margin: 72px 0;
+`
